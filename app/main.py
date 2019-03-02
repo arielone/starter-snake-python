5c2f7e93f6f1df454930e31ee324da197eec1b59
arielone <arielone@live.ca>
2019-03-02 14:58:29 -0800

    Fix running into yourself.

--- app/main.py
+++ app/main.py
@@ -62,15 +62,15 @@ def move():
     # Check if move left is valid
     if head["x"] - 1 >= 0 and head["y"] >= 0 and head["x"] - 1 < width and head["y"] < height:
         return move_response('left')
+    # Check if move down is valid
+    elif head["x"] >= 0 and head["y"] + 1 >= 0 and head["x"] < width and head["y"] + 1 < height:
+        return move_response('down')
     # Check if move right is valid
     elif head["x"] + 1 >= 0 and head["y"] >= 0 and head["x"] + 1 < width and head["y"] < height:
         return move_response('right')
     # Check if move up is valid
     elif head["x"] >= 0 and head["y"] - 1 >= 0 and head["x"] < width and head["y"] - 1 < height:
         return move_response('up')
-    # Check if move down is valid
-    elif head["x"] >= 0 and head["y"] + 1 >= 0 and head["x"] < width and head["y"] + 1 < height:
-        return move_response('down')
 
     directions = ['up', 'down', 'left', 'right']
     direction = random.choice(directions)
